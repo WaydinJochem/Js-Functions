4eb8b0d74bd43b56c1bced3259128960035602cd
WaydinJochem <jochem7405@gmail.com>
2023-05-25 12:53:01 +0200

    Work

---
+++ index.js
@@ -0,0 +1,17 @@
+let name = 'Waydin';
+function greet() {
+    console.log('Hello' + ' ' + name);
+}
+
+let wages = 3000;
+let weeks = 4;
+function multiply() {
+    console.log(wages * weeks);
+}
+
+let childSupport = 200;
+function tableOfFive() {
+    console.log(childSupport * 5);
+    return childSupport * childSupport;
+}
+
